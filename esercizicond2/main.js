30d6125e2be5968a127f71a4c341eecf4e7108d9
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-27 10:05:25 +0100

    Update main.js

--- esercizicond2/main.js
+++ esercizicond2/main.js
@@ -1,14 +1,16 @@
 let temperatura = prompt('inserisci la temperatura');
+if(temperatura == Number){
 
-if(temperatura > 0 && temperatura < 20){
+}
+ else if(temperatura > 0 && temperatura < 20){
     console.log('non ci sono piu le mezze stagioni');
-}if(temperatura >= 30){
+} else if(temperatura >= 30){
     console.log('lu mare, lu sole, lu ientu');
-}if(temperatura > 20 && temperatura < 30){
+}else if(temperatura > 20 && temperatura < 30){
     console.log('mi dia una peroni sudata');
-}if(temperatura > -10 && temperatura< 0){
+}else if(temperatura > -10 && temperatura< 0){
     console.log('non e’ tanto il freddo quanto l’umidità');
-}if(temperatura < -10){
+}else if(temperatura < -10){
     console.log('copriti…ancora ti raffreddi');
 }else {
     console.log('scrivi un numero');
